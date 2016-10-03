56c4ff5525b9f9b52f0828163c6667960a2043e0
conanOrien <ow5@gmail.com>
2016-10-02 20:34:14 -0400

    Imported Bootstrap to ./Themes; Created basic webpage for Admin in ./Site

---
+++ Site/adminHome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang=en>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="../Theme/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+        <div class="row">
+		<div class="col-md-8">
+                        <h1 style="text-align:center">Welcome to Wrong Way Airlines<br> <small> Working Hard to Exceed Delivery Standards </small></h1>
+                </div>
+                <div class="col-md-4">
+			<div>
+				<label style="text-align:center" for="flightSearch">Enter Your Cargo's Destination:</label>
+				<input type="text" class="form-control" id="flightDest" name="flightDest" placeholder="Ex: Houston, TX">
+			</div>
+			<div>
+                                <label style="text-align:center" for="flightSearch">Enter Your Cargo ID for Flight Lookup</label>
+                                <input type="number" class="form-control" id="cargoID" name="cargoID" placeholder="Ex: 12345">
+                        </div>
+
+                </div>
+        </div>
+
+</html>
